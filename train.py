bee2892d0a93707c01067f03845b654b22a930a5
u6yuvi <u6yuvi@gmail.com>
2023-05-31 22:56:11 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,47 +1,219 @@
 import os
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import argparse
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import torch.multiprocessing as mp
+
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # TODO: Define your model architecture here
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        # TODO: Define the forward pass
-        pass
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+def train(rank, args, model, device, dataset, dataloader_kwargs):
+    torch.manual_seed(args.seed + rank)
+
+    train_loader = torch.utils.data.DataLoader(dataset, **dataloader_kwargs)
+
+    optimizer = optim.SGD(
+        model.parameters(), lr=args.lr, momentum=args.momentum
+    )
+    for epoch in range(1, args.epochs + 1):
+        train_epoch(epoch, args, model, device, train_loader, optimizer)
+
+
+def test(args, model, device, dataset, dataloader_kwargs):
+    torch.manual_seed(args.seed)
+
+    test_loader = torch.utils.data.DataLoader(dataset, **dataloader_kwargs)
+
+    test_epoch(model, device, test_loader)
+
 
 def train_epoch(epoch, args, model, device, data_loader, optimizer):
-    # TODO: Implement the training loop here
-    pass
+    model.train()
+    pid = os.getpid()
+    for batch_idx, (data, target) in enumerate(data_loader):
+        optimizer.zero_grad()
+        output = model(data.to(device))
+        loss = F.nll_loss(output, target.to(device))
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print(
+                "{}\\tTrain Epoch: {} [{}/{} ({:.0f}%)]\\tLoss: {:.6f}".format(
+                    pid,
+                    epoch,
+                    batch_idx * len(data),
+                    len(data_loader.dataset),
+                    100.0 * batch_idx / len(data_loader),
+                    loss.item(),
+                )
+            )
+            if args.dry_run:
+                break
+
 
 def test_epoch(model, device, data_loader):
-    # TODO: Implement the testing loop here
-    pass
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in data_loader:
+            output = model(data.to(device))
+            test_loss += F.nll_loss(
+                output, target.to(device), reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.max(1)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.to(device)).sum().item()
+
+    test_loss /= len(data_loader.dataset)
+    print(
+        "\\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\\n".format(
+            test_loss,
+            correct,
+            len(data_loader.dataset),
+            100.0 * correct / len(data_loader.dataset),
+        )
+    )
+
 
 def main():
     # Parser to get command line arguments
-    parser = argparse.ArgumentParser(description='MNIST Training Script')
-    # TODO: Define your command line arguments here
-    
+    parser = argparse.ArgumentParser(description="MNIST Training Script")
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=1,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.01,
+        metavar="LR",
+        help="learning rate (default: 0.01)",
+    )
+    parser.add_argument(
+        "--momentum",
+        type=float,
+        default=0.5,
+        metavar="M",
+        help="SGD momentum (default: 0.5)",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=1,
+        metavar="S",
+        help="random seed (default: 1)",
+    )
+    parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=10,
+        metavar="N",
+        help="how many batches to wait before logging training status",
+    )
+    parser.add_argument(
+        "--num-processes",
+        type=int,
+        default=2,
+        metavar="N",
+        help="how many training processes to use (default: 2)",
+    )
+    parser.add_argument(
+        "--cuda",
+        action="store_true",
+        default=False,
+        help="enables CUDA training",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="quickly check a single pass",
+    )
+
     args = parser.parse_args()
     use_cuda = torch.cuda.is_available()
     torch.manual_seed(args.seed)
     device = torch.device("cuda" if use_cuda else "cpu")
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
+    dataset1 = datasets.MNIST(
+        "../data", train=True, download=True, transform=transform
+    )
+    dataset2 = datasets.MNIST("../data", train=False, transform=transform)
+    kwargs = {"batch_size": args.batch_size, "shuffle": True}
+    if use_cuda:
+        kwargs.update(
+            {"num_workers": 1, "pin_memory": True,}
+        )
 
-    # TODO: Load the MNIST dataset for training and testing
-    
+    torch.manual_seed(args.seed)
+    mp.set_start_method("spawn")
+
     model = Net().to(device)
+
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    model.share_memory()  # gradients are allocated lazily, so they are not shared here
+
+    processes = []
+    for rank in range(args.num_processes):
+        p = mp.Process(
+            target=train, args=(rank, args, model, device, dataset1, kwargs)
+        )
+        # We first train the model across `num_processes` processes
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
+
+    # Once training is complete, we can test the model
+    test(args, model, device, dataset2, kwargs)
     # TODO: Add a way to load the model checkpoint if 'resume' argument is True
 
     # TODO: Choose and define the optimizer here
-    
+
     # TODO: Implement the training and testing cycles
     # Hint: Save the model after each epoch
+
 
 if __name__ == "__main__":
     main()
